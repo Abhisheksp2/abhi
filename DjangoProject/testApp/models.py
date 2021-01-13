821850420814d59d164150714875a16f11f4dedd
Abhisheksp92 <abhishekpore73@gmail.com>
2021-01-13 23:22:56 +0530

    same here

--- DjangoProject/testApp/models.py
+++ DjangoProject/testApp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+# added comment
 class Contact(models.Model):
     name = models.CharField(max_length=50)
     mail = models.CharField(max_length=50)
